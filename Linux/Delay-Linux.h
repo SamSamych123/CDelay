910c05293d3fb87dd6c1953caea5fba4aa1091c3
SamSamych123 <varibrus.alex13@gmail.com>
2026-07-04 12:59:44 +0700

    Update Delay-Linux.h

--- Linux/Delay-Linux.h
+++ Linux/Delay-Linux.h
@@ -2,6 +2,10 @@
 #include <errno.h>
 
 void linux_delay(double second) {
+  if (second > 18446744073) {
+    second = 18446744073;
+  }
+  uint64_t nanosecond = (uint64_t)(second * 1000000000);
   time_t sec = (time_t)(nanosecond / 1000000000ULL);
   long nsec = (long)(nanosecond % 1000000000ULL);
 
